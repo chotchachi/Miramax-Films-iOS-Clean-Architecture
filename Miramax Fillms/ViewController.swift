e7f5436e2179061b1d49376dce6683bcb19cee10
Thanh Quang <quangtt@foxcode.org>
2022-09-05 20:55:26 +0700

    initial commit

---
+++ Miramax Fillms/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Miramax Fillms
+//
+//  Created by Thanh Quang on 05/09/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
