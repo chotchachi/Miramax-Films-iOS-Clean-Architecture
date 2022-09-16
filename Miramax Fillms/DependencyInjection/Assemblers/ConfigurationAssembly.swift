219e32a9d6f9b3cb7e09978f2c6f065a94dbc966
Thanh Quang <quangtt@foxcode.org>
2022-09-16 15:13:37 +0700

    [QuangTT]: - add missing configure api key NetworkConfiguration

--- Miramax Fillms/DependencyInjection/Assemblers/ConfigurationAssembly.swift
+++ Miramax Fillms/DependencyInjection/Assemblers/ConfigurationAssembly.swift
@@ -11,7 +11,7 @@ final class ConfigurationAssembly: Assembly {
     
     func assemble(container: Container) {
         container.register(NetworkConfigurationProtocol.self) { _ in
-            return NetworkConfiguration()
+            return NetworkConfiguration(apiKey: "9f7ceb7615afe6f16274a953ad31c29e")
         }
     }
 }
