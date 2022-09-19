c527f0c3d8b09c061cca74bc5929e29b80e10701
Thanh Quang <quangtt@foxcode.org>
2022-09-19 22:40:15 +0700

    [QuangTT]: - fix Genre entity

--- Miramax Fillms/Domain/Entity/Genre.swift
+++ Miramax Fillms/Domain/Entity/Genre.swift
@@ -5,13 +5,7 @@
 //  Created by Thanh Quang on 13/09/2022.
 //
 
-struct Genre {
+struct Genre: Equatable {
     let id: Int
     let name: String
-}
-
-extension Genre: Equatable {
-    static func == (lhs: Genre, rhs: Genre) -> Bool {
-        return lhs.id == rhs.id && lhs.name == rhs.name
-    }
 }
