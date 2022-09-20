77f09017d41d6a326af5c01cb8bb5d654a984234
Thanh Quang <quangtt@foxcode.org>
2022-09-20 23:52:56 +0700

    [QuangTT]: - add EntertainmentDetailModelType

---
+++ Miramax Fillms/Presentation/Common/EntertainmentDetailModelType.swift
@@ -0,0 +1,21 @@
+//
+//  EntertainmentDetailModelType.swift
+//  Miramax Fillms
+//
+//  Created by Thanh Quang on 20/09/2022.
+//
+
+import Foundation
+
+protocol EntertainmentDetailModelType {
+    var entertainmentDetailTitle: String { get }
+    var entertainmentPosterURL: URL? { get }
+    var entertainmentVoteAverage: Double { get }
+    var entertainmentRuntime: Int? { get }
+    var entertainmentReleaseDate: String { get }
+    var entertainmentOverview: String { get }
+    var entertainmentDirectors: [Crew] { get }
+    var entertainmentWriters: [Crew] { get }
+    var entertainmentCasts: [Cast] { get }
+    var entertainmentRecommends: [EntertainmentModelType] { get }
+}
