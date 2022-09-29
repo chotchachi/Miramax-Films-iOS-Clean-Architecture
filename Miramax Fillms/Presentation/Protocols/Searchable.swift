792b103cd9e03ee85885530da695cc5d6a38bbba
Thanh Quang <quangtt@foxcode.org>
2022-09-29 23:00:06 +0700

    [QuangTT]: - add Searchable + SearchButton

---
+++ Miramax Fillms/Presentation/Protocols/Searchable.swift
@@ -0,0 +1,12 @@
+//
+//  Searchable.swift
+//  Miramax Fillms
+//
+//  Created by Thanh Quang on 29/09/2022.
+//
+
+import UIKit
+
+protocol Searchable: AnyObject {
+    var btnSearch: SearchButton { get }
+}
---
+++ Miramax Fillms/Presentation/ViewComponents/SearchButton/SearchButton.swift
@@ -0,0 +1,27 @@
+//
+//  SearchButton.swift
+//  Miramax Fillms
+//
+//  Created by Thanh Quang on 29/09/2022.
+//
+
+import UIKit
+
+final class SearchButton: UIButton {
+    
+    // MARK: - Initializers
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        setup()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setup() {
+        setImage(UIImage(named: "ic_toolbar_search"), for: .normal)
+    }
+}
