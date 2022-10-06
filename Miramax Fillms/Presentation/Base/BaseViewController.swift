d84ad443a7502226c4bd42ed103002aea12a1ecd
Thanh Quang <thanhquang.stahli@gmail.com>
2022-10-06 23:56:55 +0700

    [QuangTT]: - add BaseViewController gradient background

--- Miramax Fillms/Presentation/Base/BaseViewController.swift
+++ Miramax Fillms/Presentation/Base/BaseViewController.swift
@@ -8,10 +8,11 @@
 import UIKit
 import RxSwift
 import RxCocoa
+import SnapKit
 
 class BaseViewController<T: BaseViewModel>: UIViewController {
     var viewModel: T!
-        
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -20,13 +21,27 @@ class BaseViewController<T: BaseViewModel>: UIViewController {
     }
     
     func configView() {
-        
+        setGradientBackground()
     }
     
     func bindViewModel() {
         rx.sentMessage(#selector(UIViewController.viewDidAppear(_:)))
             .mapToVoid()
             .bind(to: viewModel.trigger)
-            .disposed(by: rx.disposeBag)        
+            .disposed(by: rx.disposeBag)
+    }
+    
+    private func setGradientBackground() {
+        let gradientView = GradientView()
+        gradientView.translatesAutoresizingMaskIntoConstraints = false
+        gradientView.startColor = AppColors.colorPrimary
+        gradientView.endColor = AppColors.colorSecondary
+        view.insertSubview(gradientView, at: 0)
+        gradientView.snp.makeConstraints { make in
+            make.top.equalToSuperview()
+            make.bottom.equalToSuperview()
+            make.leading.equalToSuperview()
+            make.trailing.equalToSuperview()
+        }
     }
 }
