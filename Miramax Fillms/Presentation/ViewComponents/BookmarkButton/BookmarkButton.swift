29b41aad91aab1e5d8307fe014c1b33ec3a92de8
Thanh Quang <thanhquang.stahli@gmail.com>
2022-10-08 21:09:03 +0700

    [QuangTT]: - add BookmarkButton

---
+++ Miramax Fillms/Presentation/ViewComponents/BookmarkButton/BookmarkButton.swift
@@ -0,0 +1,61 @@
+//
+//  BookmarkButton.swift
+//  Miramax Fillms
+//
+//  Created by Thanh Quang on 08/10/2022.
+//
+
+import UIKit
+
+@IBDesignable
+final class BookmarkButton: UIButton {
+    
+    // MARK: - Properties
+    
+    @IBInspectable var isBookmark: Bool = true {
+        didSet {
+            setBookmarkIcon()
+        }
+    }
+    
+    override var isHighlighted: Bool {
+        didSet {
+            alpha = isHighlighted ? 0.5 : 1.0
+        }
+    }
+
+    // MARK: - Initializers
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        setup()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+       super.init(coder: aDecoder)
+        
+        setup()
+    }
+    
+    override var intrinsicContentSize: CGSize {
+        return .init(width: 36.0, height: 36.0)
+    }
+    
+    override func prepareForInterfaceBuilder() {
+        invalidateIntrinsicContentSize()
+    }
+    
+    private func setup() {
+        translatesAutoresizingMaskIntoConstraints = false
+        
+        setTitle("", for: .normal)
+        
+        setBookmarkIcon()
+    }
+    
+    private func setBookmarkIcon() {
+        let icon = isBookmark ? UIImage(named: "ic_bookmark_fill") : UIImage(named: "ic_unbookmark_fill")
+        setImage(icon, for: .normal)
+    }
+}
